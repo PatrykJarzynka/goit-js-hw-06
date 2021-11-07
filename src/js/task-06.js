405bc9e4af707448f73cdc46e2c5c5b10ca38d1c
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-11-07 22:52:16 +0100

    Poprawiono zadanie 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,8 +3,8 @@ let input = document.querySelector('#validation-input');
 const changeClasses = () => {
   input.addEventListener('focus', () => input.classList.remove('valid', 'invalid'));
   input.addEventListener('blur', () => {
-    if (input.value.length === 6) input.classList.add('valid');
-    else if (input.value.length !== 6) input.classList.add('invalid');
+    if (input.value.length == input.dataset.length) input.classList.add('valid');
+    else if (input.value.length !== input.dataset.length) input.classList.add('invalid');
   });
 };
 
